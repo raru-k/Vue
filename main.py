72e4efc2e841d239249ed95755b12b355b281209
raru_k <r.0324.i@icloud.com>
2023-05-10 19:14:02 +0900

    5-10

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def todo():
     df = pd.read_csv('../static/todo.csv', header=None, encoding='CP932')
+
+@app.route('\create')
+def create():
+    return render_template('create.html')
