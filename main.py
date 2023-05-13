6af7a94055f1913607e1f550d6f2490574587462
raru_k <r.0324.i@icloud.com>
2023-05-14 02:34:35 +0900

    kouka

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-from flask import Flask, render_template, jsonify
-import pandas as pd
+from flask import Flask, render_template,request
+
+def updateTxt(text):
+    if(text is None):
+        return
+    with open("static/txt/todo.txt","w",encoding="utf-8") as f:
+        f.write(text)
+
+def readTxt():
+    with open("static/txt/todo.txt","r",encoding="utf-8") as f:
+        text = f.read()
+    if text == "":
+        text = '[]'
+    return text
 
 app = Flask(__name__)
 
-
-
-@app.route('/')
-def index():
-    df = pd.read_csv('static/csv/todo.csv', header=None, encoding='utf-8')
+@app.route('/todo-list', methods=['GET', 'POST'])
+def updateToDo():
     
-    df.columns = ['todo','check','important','visible']
+    if request.method == 'POST': # POSTの場合はtxtファイルの中身を更新する
+        
+        print(request.form.get("tasks"))
+        
+        updateTxt(request.form.get("tasks"))
     
-    ToDoList = []
-    
-    for i in range(len(df)):
-        ToDoList.append({
-            "todo" : df['todo'][i],
-            "check" : df['check'][i],
-            "important" : df['important'][i],
-            "visible" : df['visible'][i]
-        })
+    list= readTxt()
     
     return render_template('index.html',
-                        ToDoList=ToDoList)
-
+                            list=list)
 
 
 if __name__ == '__main__':
